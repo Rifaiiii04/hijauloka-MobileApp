d3efed9c94859985dd64eae8fb3816651471ef21
Rifaiiii04 <muhamadrifay989@gmail.com>
2025-03-16 14:18:24 +0700

    ,

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'login_screen.dart';
+import 'package:image_picker/image_picker.dart'; // Untuk memilih gambar
+import 'dart:io'; // Untuk menangani file gambar
+import 'package:http/http.dart' as http; // Untuk mengupload gambar
+import 'dart:convert'; // Untuk encode/decode JSON
+import 'login_screen.dart'; // Untuk logout
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   final String userEmail;
   final String userName;
 
@@ -10,6 +14,90 @@ class ProfileScreen extends StatelessWidget {
     required this.userEmail,
     required this.userName,
   });
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  File? _profileImage; // File gambar profil
+  final ImagePicker _picker = ImagePicker(); // Untuk memilih gambar
+
+  // Fungsi untuk memilih gambar dari galeri
+  Future<void> _pickImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _profileImage = File(pickedFile.path);
+      });
+      // Upload gambar ke server
+      await _uploadImage(_profileImage!);
+    }
+  }
+
+  // Fungsi untuk mengambil gambar dari kamera
+  Future<void> _takeImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
+
+    if (pickedFile != null) {
+      setState(() {
+        _profileImage = File(pickedFile.path);
+      });
+      // Upload gambar ke server
+      await _uploadImage(_profileImage!);
+    }
+  }
+
+  // Fungsi untuk menghapus gambar profil
+  Future<void> _deleteImage() async {
+    setState(() {
+      _profileImage = null;
+    });
+    // Hapus gambar dari server
+    await _deleteImageFromServer();
+  }
+
+  // Fungsi untuk mengupload gambar ke server
+  Future<void> _uploadImage(File image) async {
+    final url =
+        Uri.parse('http://localhost/p_hijauloka/upload_profile_image.php');
+    final request = http.MultipartRequest('POST', url)
+      ..files
+          .add(await http.MultipartFile.fromPath('profile_image', image.path))
+      ..fields['email'] = widget.userEmail;
+
+    final response = await request.send();
+
+    if (response.statusCode == 200) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Foto profil berhasil diupload!')),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Gagal mengupload foto profil!')),
+      );
+    }
+  }
+
+  // Fungsi untuk menghapus gambar dari server
+  Future<void> _deleteImageFromServer() async {
+    final url =
+        Uri.parse('http://localhost/p_hijauloka/delete_profile_image.php');
+    final response = await http.post(url, body: {
+      'email': widget.userEmail,
+    });
+
+    if (response.statusCode == 200) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Foto profil berhasil dihapus!')),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Gagal menghapus foto profil!')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,17 +155,87 @@ class ProfileScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const SizedBox(height: 20),
-              ClipOval(
-                child: Image.asset(
-                  'assets/profile.JPG',
-                  width: 120,
-                  height: 120,
-                  fit: BoxFit.cover,
-                ),
+              Stack(
+                children: [
+                  ClipOval(
+                    child: _profileImage != null
+                        ? Image.file(
+                            _profileImage!,
+                            width: 120,
+                            height: 120,
+                            fit: BoxFit.cover,
+                          )
+                        : Container(
+                            width: 120,
+                            height: 120,
+                            color: Colors.grey[200],
+                            child: const Icon(
+                              Icons.person,
+                              size: 60,
+                              color: Colors.grey,
+                            ),
+                          ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 3,
+                            offset: const Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: IconButton(
+                        icon: const Icon(Icons.camera_alt,
+                            color: Color(0xFF08644C)),
+                        onPressed: () {
+                          showModalBottomSheet(
+                            context: context,
+                            builder: (context) => Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                ListTile(
+                                  leading: const Icon(Icons.photo_library),
+                                  title: const Text('Pilih dari Galeri'),
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                    _pickImage();
+                                  },
+                                ),
+                                ListTile(
+                                  leading: const Icon(Icons.camera_alt),
+                                  title: const Text('Ambil Foto'),
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                    _takeImage();
+                                  },
+                                ),
+                                ListTile(
+                                  leading: const Icon(Icons.delete),
+                                  title: const Text('Hapus Foto Profil'),
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                    _deleteImage();
+                                  },
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ],
               ),
               const SizedBox(height: 10),
               Text(
-                userName, // Nama user yang login
+                widget.userName,
                 style: const TextStyle(
                   fontWeight: FontWeight.w500,
                   fontFamily: "Poppins",
